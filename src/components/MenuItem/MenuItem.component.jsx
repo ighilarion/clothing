a506e8d9427f8c3cf16fa32b9b0532bf5618597a
ighilarion <ighilarion.1990@gmail.com>
2021-03-01 13:24:39 +0100

    puse los componentes

---
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import "./MenuItem.style.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    ></div>
+
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/HomePage/HomePage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.style.scss";
+import DirectoryMenu from "../../components/DirectoryMenu/DirectoryMenu.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirectoryMenu />
+  </div>
+);
+
+export default HomePage;
